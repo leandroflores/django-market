abf374a03b8e11a5208e0e104dca76b93201d626
leandroflores7 <leandroflores7@gmail.com>
2024-10-01 18:25:06 -0300

    Add tests

--- market_api/market/clients/tests.py
+++ market_api/market/clients/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.http import HttpResponse
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Customer
+
+
+class TestCustomerAPIView(APITestCase):
+
+    def setUp(self) -> None:
+        self.valid_input: dict = {
+            "document": "2313131313",
+            "name": "John",
+            "type": "F",
+        }
+        self.customer = Customer.objects.create(**self.valid_input)
+        
+    
+    def test_get_customers(self):
+        url: str = "/customers/"
+        response: HttpResponse = self.client.get(url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.content, {})
+    
